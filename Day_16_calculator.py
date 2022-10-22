a31df8e6d7900348193d2d3e7d05c72321ef0f90
Aaryan Anil <90103645+CodeLikeAaryan@users.noreply.github.com>
2022-10-22 20:16:19 +0530

    Delete Day_16_calculator.py

--- Day_16_calculator.py
+++
@@ -1,5 +0,0 @@
-# Write a python code to add two numbers using input function
-a = int(input("Enter the first number "))
-b = int(input("Enter the second number "))
-result = a+b
-print(f"The Value obtained by adding a and b is {result}")
