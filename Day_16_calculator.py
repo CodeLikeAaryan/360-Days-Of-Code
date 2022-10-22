bd1f8030f9c7c55c83db297a0879a6dc9dc1ddc5
Aaryan Anil <90103645+CodeLikeAaryan@users.noreply.github.com>
2022-10-22 20:15:58 +0530

    Add files via upload

---
+++ Day_16_calculator.py
@@ -0,0 +1,5 @@
+# Write a python code to add two numbers using input function
+a = int(input("Enter the first number "))
+b = int(input("Enter the second number "))
+result = a+b
+print(f"The Value obtained by adding a and b is {result}")
---
+++ Day_17_reverse_string.py
@@ -0,0 +1,4 @@
+# Implement a python code to reverse a string
+mystr = input("Enter a String of your choice")
+result = mystr[::-1]
+print(f"The Reverse String Obtained is {result}")
