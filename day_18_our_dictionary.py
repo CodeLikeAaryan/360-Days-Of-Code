2dd348c5174f193b0a25c832182b594ebdf8ade8
Aaryan Anil <90103645+CodeLikeAaryan@users.noreply.github.com>
2022-10-23 22:59:56 +0530

    Add files via upload

---
+++ day_18_our_dictionary.py
@@ -0,0 +1,8 @@
+# Create a dictionary to take input from the user and and return the meaning of the word from the dictionary
+my_dict = {
+    "fast": "quick",
+    "hold": "keep",
+    "aaryan": "good boy",
+}
+inp_val = input("Enter the word ")
+result = print(f"The meaning of the word is {my_dict[inp_val]} ")
